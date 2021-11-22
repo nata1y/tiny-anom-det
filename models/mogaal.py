910860f72151fb1c1afbf8950d0a4ee71f24ffcb
nata1y <oxi-flent@mail.ru>
2021-11-22 16:18:05 +0100

    MODIFY Mogaal to lstm layers

--- models/mogaal.py
+++ models/mogaal.py
@@ -10,6 +10,11 @@ import math
 import argparse
 from tensorflow.keras.initializers import VarianceScaling, Identity
 import tensorflow
+from tensorflow.python.keras.engine.keras_tensor import KerasTensor
+from tensorflow.python.keras.layers import TimeDistributed, Dropout, LSTM, RepeatVector
+
+from utils import create_dataset, create_dataset_keras
+anomaly_window = 60
 
 print(f'Running on GPU {tensorflow.test.is_built_with_cuda()}. '
       f'Devices: {tensorflow.config.list_physical_devices("GPU")}')
@@ -21,7 +26,7 @@ def parse_args():
                         help='Input data path.')
     parser.add_argument('--k', type=int, default=10,
                         help='Number of sub_generator.')
-    parser.add_argument('--stop_epochs', type=int, default=1500,
+    parser.add_argument('--stop_epochs', type=int, default=500,
                         help='Stop training generator after stop_epochs.')
     parser.add_argument('--lr_d', type=float, default=0.01,
                         help='Learning rate of discriminator.')
@@ -36,7 +41,7 @@ def parse_args():
 
 class MOGAAL:
 
-    def __init__(self, args=None):
+    def __init__(self, dataset, args=None):
         if not args:
             args = parse_args()
         self.decay = args.decay
@@ -46,26 +51,41 @@ class MOGAAL:
         self.momentum = args.momentum
         self.path = args.path
         self.stop_epochs = args.stop_epochs
+        self.dataset, self.type, self.file = dataset
 
     # Generator
     def create_generator(self, latent_size):
         gen = Sequential()
-        gen.add(Dense(latent_size, input_dim=latent_size, activation='relu', kernel_initializer=Identity(gain=1.0)))
+        gen.add(Dense(latent_size, input_dim=self.latent_size, activation='relu', kernel_initializer=Identity(gain=1.0)))
         gen.add(Dense(latent_size, activation='relu', kernel_initializer=Identity(gain=1.0)))
-        latent = Input(shape=(latent_size,))
+        latent = Input(shape=(self.latent_size,))
         fake_data = gen(latent)
         return Model(latent, fake_data)
 
     # Discriminator
     def create_discriminator(self):
         dis = Sequential()
-        dis.add(Dense(math.ceil(math.sqrt(self.data_size)), input_dim=self.latent_size, activation='relu',
-                      kernel_initializer=VarianceScaling(scale=1.0, mode='fan_in', distribution='normal', seed=None)))
-        dis.add(Dense(1, activation='sigmoid', kernel_initializer=VarianceScaling(scale=1.0, mode='fan_in',
-                                                                                  distribution='normal', seed=None)))
-        data = Input(shape=(self.latent_size,))
+        dis.add(LSTM(128, input_shape=(anomaly_window, 1)))
+        dis.add(Dropout(rate=0.2))
+        dis.add(RepeatVector(anomaly_window))
+        dis.add(LSTM(128, return_sequences=True))
+        dis.add(Dropout(rate=0.2))
+        dis.add(TimeDistributed(Dense(1)))
+        dis.compile(optimizer='adam', loss='mae')
+        data = Input(shape=(anomaly_window, 1))
         fake = dis(data)
         return Model(data, fake)
+
+        # dis = Sequential()
+        # dis.add(Dense(math.ceil(math.sqrt(self.data_size)), input_dim=self.latent_size, activation='relu',
+        #               kernel_initializer=tensorflow.keras.initializers.VarianceScaling(scale=1.0, mode='fan_in',
+        #                                                                     distribution='normal', seed=None)))
+        # dis.add(Dense(1, activation='sigmoid',
+        #               kernel_initializer=tensorflow.keras.initializers.VarianceScaling(scale=1.0, mode='fan_in',
+        #                                                                     distribution='normal', seed=None)))
+        # data = Input(shape=(self.latent_size,))
+        # fake = dis(data)
+        # return Model(data, fake)
 
     # Load data
     def load_data(self):
@@ -87,12 +107,18 @@ class MOGAAL:
             self.names['gy_' + str(i)] = train_history['sub_generator{}_loss'.format(i)]
         x = np.linspace(1, len(dy), len(dy))
         fig, ax = plt.subplots()
-        ax.plot(x, dy, color='blue')
-        ax.plot(x, gy,color='red')
-        ax.plot(x, auc_y, color='yellow', linewidth = '3')
+        ax.plot(x, dy, color='blue', label='Discriminator loss')
+        ax.plot(x, gy, color='red', label='Generator loss')
+        ax.plot(x, auc_y, color='yellow', linewidth='3', label='ROC AUC')
         for i in range(self.k):
-            ax.plot(x, self.names['gy_' + str(i)], color='green', linewidth='0.5')
-        plt.show()
+            ax.plot(x, self.names['gy_' + str(i)], color='green', linewidth='0.5', label='Sub-generator loss')
+        plt.legend()
+        plt.savefig(f'results/imgs/{self.dataset}/{self.type}/mogaal/mogaal_{self.file.replace(".csv", "")}_loss.png')
+
+        outliers = []
+        # save potential outliers
+        for i in range(self.k):
+            outliers.append(self.names['generated_data' + str(i)])
 
     def predict(self, data=pd.DataFrame([])):
         if data.shape[0] == 0:
@@ -111,17 +137,31 @@ class MOGAAL:
 
         return preds
 
-    def fit(self, data=pd.DataFrame([]), plot=True, batch_size=500):
+    def _insert_anomalies(self, data_batch, anomalies, noise_idxs):
+        data_batch.set_index('timestamp', inplace=True)
+        # TODO: check whether output indeed normalized
+        for (val, t) in anomalies:
+            if int(t) in data_batch.index:
+                data_batch.loc[int(t), 'value'] = val
+                noise_idxs.append(int(t))
+        data_batch.reset_index(inplace=True)
+        return data_batch
+
+    def fit(self, data=pd.DataFrame([]), plot=True, batch_size=60):
         self.train = True
 
         # initialize dataset
         if data.shape[0] == 0:
             data_x, data_y, data_id = self.load_data()
         else:
-            data_x, data_y, data_id = data[['value']].values, data['is_anomaly'].values, data['timestamp'].values
+            data_x, data_y, data_id = data[['value', 'timestamp']], data['is_anomaly'].values, data['timestamp'].values
+
         self.data_size = data_x.shape[0]
         self.latent_size = data_x.shape[1]
         print("The dimension of the training data :{}*{}".format(self.data_size, self.latent_size))
+        self.last_batch_anomalies = []
+
+        # data_x, _ = create_dataset(data_x[['value']], data_x[['value']], anomaly_window)
 
         if self.train:
             train_history = defaultdict(list)
@@ -139,11 +179,17 @@ class MOGAAL:
             for i in range(k):
                 self.names['sub_generator' + str(i)] = self.create_generator(self.latent_size)
                 latent = Input(shape=(self.latent_size,))
-                self.names['fake' + str(i)] = self.names['sub_generator' + str(i)](latent)
+                self.names['fake' + str(i)] = tensorflow.expand_dims(tensorflow.gather(
+                    self.names['sub_generator' + str(i)](latent), [0], axis=1), axis=0)
                 self.discriminator.trainable = False
-                self.names['fake' + str(i)] = self.discriminator(self.names['fake' + str(i)])
+
+                tsvalues = tensorflow.expand_dims(tensorflow.gather(self.names['fake' + str(i)], [0], axis=1), axis=0)
+                # X, y = create_dataset_keras(tsvalues, tsvalues, 60)
+                self.names['fake' + str(i)] = self.discriminator(tsvalues) #r(self.names['fake' + str(i)])
                 self.names['combine_model' + str(i)] = Model(latent, self.names['fake' + str(i)])
-                self.names['combine_model' + str(i)].compile(optimizer=SGD(lr=self.lr_g, decay=self.decay, momentum=self.momentum), loss='binary_crossentropy')
+                self.names['combine_model' + str(i)].compile(optimizer=
+                                                             SGD(lr=self.lr_g, decay=self.decay, momentum=self.momentum),
+                                                             loss='binary_crossentropy')
 
             # Start iteration
             for epoch in range(epochs):
@@ -156,7 +202,11 @@ class MOGAAL:
 
                     # Generate noise
                     noise_size = batch_size
-                    noise = np.random.uniform(0, 1, (int(noise_size), self.latent_size))
+                    # Normalization does not make sense?
+                    # noise = np.random.uniform(0, 1, (int(noise_size), self.latent_size))
+                    noise = np.random.uniform(data['value'].min(), data['value'].max(), (int(noise_size), 1))
+                    # append artificial time index
+                    noise = np.append(noise, [[x] for x in range(index * batch_size, (index + 1) * batch_size)], axis=1)
 
                     # Get training data
                     data_batch = data_x[index * batch_size: (index + 1) * batch_size]
@@ -175,20 +225,39 @@ class MOGAAL:
                             self.names['generated_data' + str(i)] = self.names['sub_generator' + str(i)].predict(self.names['noise' + str(i)], verbose=0)
 
                     # Concatenate real data to generated data
+                    noise_idxs = []
                     for i in range(k):
                         if i == 0:
-                            X = np.concatenate((data_batch, self.names['generated_data' + str(i)]))
+                            X = self._insert_anomalies(data_batch, self.names['generated_data' + str(i)], noise_idxs)
+                            # X = np.concatenate((data_batch, self.names['generated_data' + str(i)]))
                         else:
-                            X = np.concatenate((X, self.names['generated_data' + str(i)]))
-                    Y = np.array([1] * batch_size + [0] * int(noise_size))
+                            # TODO: shall we insert anomalies in last batch only or in all X history?
+                            X = self._insert_anomalies(X, self.names['generated_data' + str(i)], noise_idxs)
+                            # X = np.concatenate((X, self.names['generated_data' + str(i)]))
+
+                    # instead of concatenating, we insert anomalies inside of time strean
+                    # Y = np.array([1] * batch_size + [0] * int(noise_size))
+                    Y = np.array([1 if x in noise_idxs else 0 for x in X.index.tolist()])
 
                     # Train discriminator
+                    X = X['value'].to_numpy().reshape(1, anomaly_window, 1)
+                    Y = Y.reshape(1, anomaly_window)
                     discriminator_loss = self.discriminator.train_on_batch(X, Y)
                     train_history['discriminator_loss'].append(discriminator_loss)
+                    # ==================================================================================================
 
                     # Get the target value of sub-generator
+                    threshold = 0.5
+                    data_x, _ = create_dataset(data_x[['value']], data_x[['value']], anomaly_window)
                     p_value = self.discriminator.predict(data_x)
-                    p_value = pd.DataFrame(p_value)
+
+                    # transform lstm batched predictions to normal ones
+                    loss = np.abs(X - p_value).ravel()[:X.shape[1]]
+                    y_pred = [0 if loss[idx] <= threshold else 1 for idx in range(len(loss))]
+
+                    # p_value = pd.DataFrame(p_value)
+                    p_value = pd.DataFrame(loss)
+
                     for i in range(k):
                         self.names['T' + str(i)] = p_value.quantile(i/k)
                         self.names['trick' + str(i)] = np.array([float(self.names['T' + str(i)])] * noise_size)
@@ -197,13 +266,21 @@ class MOGAAL:
                     noise = np.random.uniform(0, 1, (int(noise_size), self.latent_size))
                     if stop == 0:
                         for i in range(k):
+                            noise = noise[:, 0].reshape(1, anomaly_window, 1)
+                            self.names['trick' + str(i)] = np.array([[x] for x in self.names['trick' + str(i)]])
+                            self.names['trick' + str(i)] = np.append(
+                                self.names['trick' + str(i)], [[x] for x in range(index * batch_size, (index + 1)
+                                                                                  * batch_size)], axis=1)
+                            self.names['trick' + str(i)] = self.names['trick' + str(i)].reshape(1,
+                                                                                                self.names['trick' + str(i)].shape[0],
+                                                                                                self.names['trick' + str(i)].shape[1])
                             self.names['sub_generator' + str(i) + '_loss'] = self.names['combine_model' +
                                                                                         str(i)].train_on_batch(noise, self.names['trick' + str(i)])
-                            train_history['sub_generator{}_loss'.format(i)].append(self.names['sub_generator' + str(i) + '_loss'])
+                            train_history[f'sub_generator{i}_loss'].append(self.names['sub_generator' + str(i) + '_loss'])
                     else:
                         for i in range(k):
                             self.names['sub_generator' + str(i) + '_loss'] = self.names['combine_model' + str(i)].evaluate(noise, self.names['trick' + str(i)])
-                            train_history['sub_generator{}_loss'.format(i)].append(self.names['sub_generator' + str(i) + '_loss'])
+                            train_history[f'sub_generator{i}_loss'].append(self.names['sub_generator' + str(i) + '_loss'])
 
                     generator_loss = 0
                     for i in range(k):
@@ -221,13 +298,10 @@ class MOGAAL:
                     result = np.concatenate((p_value, data_y), axis=1)
                     result = pd.DataFrame(result, columns=['p', 'y'])
                     result = result.sort_values('p', ascending=True)
-                    print(result)
 
                     # Calculate the AUC
                     inlier_parray = result.loc[lambda df: df.y == 0.0, 'p'].values
                     outlier_parray = result.loc[lambda df: df.y == 1.0, 'p'].values
-                    print(inlier_parray)
-                    print(outlier_parray)
                     sum = 0.0
                     for o in outlier_parray:
                         for i in inlier_parray:
